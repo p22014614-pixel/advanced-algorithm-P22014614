ba777bc322580404a112f7b1ca46ea9d50e597b3
alvans <P22014614@student.newinti.edu.my>
2026-06-26 15:29:21 +0800

    main controller program to run both factorial calculation methods

---
+++ Individual_Assignment/System3_MultithreadingFactorial/main.py
@@ -0,0 +1,17 @@
+from multithreading_test import multithreading_experiment
+
+from normal_test import normal_experiment
+
+
+
+print("==============================")
+print(" Factorial Multithreading Test")
+print("==============================")
+
+
+
+multithreading_experiment()
+
+
+
+normal_experiment()
