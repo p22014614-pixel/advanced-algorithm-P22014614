a5e0937fc21e3e2b7c568031ca7c68d2f6546058
alvans <P22014614@student.newinti.edu.my>
2026-06-26 15:34:07 +0800

    first calculation is too fast , thread creation overhead makes multithreading slower , so new functions are implemented called repeated_factorial

--- Individual_Assignment/System3_MultithreadingFactorial/factorial.py
+++ Individual_Assignment/System3_MultithreadingFactorial/factorial.py
@@ -1,7 +1,20 @@
 def factorial(number):
+
     result = 1
 
     for i in range(1, number + 1):
         result = result * i
+
+    return result
+
+
+
+def repeated_factorial(number, repeat):
+
+    result = None
+
+    for i in range(repeat):
+
+        result = factorial(number)
 
     return result
